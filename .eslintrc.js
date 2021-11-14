030637894c399ba746d7752595b5666eb429d9ed
zhouyu <rudy_zhou@sian.cn>
2021-11-14 17:17:55 +0800

    修改console 验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", 'plugin:vue/recommended', "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
